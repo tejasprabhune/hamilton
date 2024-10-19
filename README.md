4ed3afa9466f14cb8341abab3bebbbd05c419af9
Ayushi Batwara <ayushibatwara@gmail.com>
2024-10-19 13:14:26 -0700

    scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hamilton
+
+1. Create a Python virtual environment `python3 -m venv venv`
+2. Run `pip install -r requirements.txt`
+3. Run the backend by cd-ing into the backend directory and run `python3 server.py`
+
+Before you push your code, just run `pip freeze > requirements.txt`
---
+++ backend/server.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+@app.route('/initial-agreement-level')
+def initial_agreement_level():
+    """
+    Input: takes in a clause in plaintext
+    Output: ids of the min and max agreement senators
+    """
+    pass
+
+@app.route('/debate')
+def debate():
+    """
+    Input: senator id, current clause
+    Output: updated clause 
+    """
+    pass
+
+@app.route('/vote')
+def vote():
+    """
+    Input: updated clause
+    Output: whether or not majority have agreed 
+    """
+
+@app.route
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
